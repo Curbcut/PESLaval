67951235b05081f27d29ba3537bfdaa0d8b56fa3
Justin Le <justin.le2@mail.mcgill.ca>
2024-05-29 20:43:37 -0400

    Bixi stations, bus stop locations, cycle paths

--- R/15_scripts/mobility_transport.R
+++ R/15_scripts/mobility_transport.R
@@ -1,2 +1,90 @@
 #Loading up libraries
 source("R/01_startup.R")
+library(gbfs)
+library(sf)
+library(tidytransit)
+library(osmdata)
+
+#Setting up the cancensus api key
+set_cancensus_api_key("CensusMapper_4308d496f011429cf814385050f083dc", install = TRUE)
+
+#Setting up cache path for faster loading (edit for your own folder)
+set_cancensus_cache_path("D:/McGill/can_cache", install = TRUE, overwrite = TRUE)
+
+#Grabbing the Laval shapefile by CT
+laval_ct <- cancensus::get_census(dataset = "CA21", 
+                                  regions = list(CSD = 2465005), 
+                                  level = "CT", 
+                                  geo_format = "sf")
+
+mtl_ct <- cancensus::get_census(dataset = "CA21", 
+                                regions = list(CSD = 2466023), 
+                                level = "CSD", 
+                                geo_format = "sf")
+
+# Bus Stop Location -------------------------------------------------------
+#Grabbing GTFS data from the STL
+stl_gtfs <- read_gtfs("https://translate.google.com/website?sl=fr&tl=en&hl=en&prev=search&u=https://www.stlaval.ca/datas/opendata/GTF_STL.zip")
+
+#Grabbing bus stops, converting it to be usable to plot, and removing duplicates
+#Removing duplicates doesn't necessarily reflect real life as there are usually
+#two stops at one intersection, but makes map readability significantly better
+bus_stops <- st_as_sf(stl_gtfs$stops, coords = c("stop_lon", "stop_lat"), crs = 4326) |> 
+  distinct(stop_name, .keep_all = TRUE)
+
+laval_bbox <- st_bbox(laval_ct)
+
+#Plotting the bus stops
+ggplot() +
+  geom_sf(data = laval_ct, fill = "white", color = "black") +
+  geom_sf(data = mtl_ct, fill = "lightgrey", color = "black") +
+  geom_sf(data = bus_stops, color = "indianred3", size = 0.65) +
+  theme_minimal() +
+  labs(title = "Société de transport de Laval Bus Stops 2024") +
+  theme(plot.title = element_text(hjust = 0.5), axis.text = element_blank(),
+        axis.title = element_blank(), axis.ticks = element_blank(),
+        panel.grid = element_blank()) +
+  coord_sf(xlim = c(laval_bbox$xmin, laval_bbox$xmax),
+           ylim = c(laval_bbox$ymin, laval_bbox$ymax))
+
+# Cycle Paths -------------------------------------------------------------
+laval_bbox <- st_bbox(laval_ct)
+
+cycle_osm <- opq(bbox = laval_bbox) |> 
+  add_osm_feature(key = "highway", value = "cycleway") |> 
+  osmdata_sf()
+
+cycle_lvl <- cycle_osm$osm_lines |> 
+  st_intersection(laval_ct)
+
+ggplot() +
+  geom_sf(data = laval_ct, fill = "white", color = "black") +
+  geom_sf(data = cycle_lvl, color = "darkred", size = 5) +
+  coord_sf() +
+  theme_minimal() +
+  labs(title = "Cycle Paths in Laval 2024") +
+  theme(plot.title = element_text(hjust = 0.5), axis.text = element_blank(),
+        axis.title = element_blank(), axis.ticks = element_blank(),
+        panel.grid = element_blank()) +
+  coord_sf(xlim = c(laval_bbox$xmin, laval_bbox$xmax),
+           ylim = c(laval_bbox$ymin, laval_bbox$ymax))
+# Bixi Stations -----------------------------------------------------------
+#Grabbing all Bixi station information
+bixi_stations <- get_station_information(city = "Montreal")
+
+#Converting the bixi station information into a usable coordinate system
+bixi_sf <- st_as_sf(bixi_stations, coords = c("lon", "lat"), crs = 4326) |> 
+  filter(name != "cpatel") |> 
+  st_intersection(laval_ct)
+
+#Mapping out the stations onto the city of Laval
+ggplot() +
+  geom_sf(data = laval_ct, fill = "white", color = "black") +
+  geom_sf(data = bixi_sf, color = "red", size = 1) +
+  theme_minimal() +
+  labs(title = "Bixi Stations in Laval 2024") +
+  theme(plot.title = element_text(hjust = 0.5),
+        axis.text = element_blank(),
+        axis.title = element_blank(),
+        axis.ticks = element_blank(),
+        panel.grid = element_blank())
