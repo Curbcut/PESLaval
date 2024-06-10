42133fdb1840b5fa5518e69e110c337fcd485076
Justin Le <justin.le2@mail.mcgill.ca>
2024-06-10 12:58:38 -0400

    Grabbing MTL CMA shapefile, green spaces, and new flood data

--- R/13_scripts/climate_change.R
+++ R/13_scripts/climate_change.R
@@ -18,7 +18,13 @@ can21 <- list_census_vectors(dataset = "CA21")
 set_cancensus_cache_path("D:/McGill/can_cache", install = TRUE, overwrite = TRUE)
 set_cmhc_cache_path("D:/McGill/can_cache", install = TRUE, overwrite = TRUE)
 
-#Grabbing Laval's shapefile by census tract
+#Reading the Montreal CMA shapefile
+mtlcma_sf <- cancensus::get_census(dataset = "CA21",
+                                   regions = list(CMA = 24462),
+                                   level = "CMA",
+                                   geo_format = "sf")
+
+#Grabbing Laval's shapefile by census subdivision
 laval_csd <- cancensus::get_census(dataset = "CA21", 
                                   regions = list(CSD = 2465005), 
                                   level = "CSD", 
@@ -91,23 +97,48 @@ ggplot(data = ouranos_sf) +
                              label.position = "bottom", keywidth = 3, keyheight = 0.5))
 
 # Biodiversity ------------------------------------------------------------
-
+#Reading greenspace shapefile and canopy .tif
+greenspace_shp <- st_read("/Users/justin/Documents/R/curbcut/parc_espace_vert_20240607_PG.shp")
 canopy_tif <- raster("D://Mcgill/can_cache/treecanopy/treecanopy.tif")
 
+#Adding artificial margins, breaks, and associated colors
 par(mar = c(0, 0, 0, 0))
 breaks <- c(0, 1, 2, 3, 4, 5)
 color_breaks <- c("#feb24c", "#fff7bc", "#e5f5f9", "#99d8c9", "#a6bddb")
 
+#Plotting the .tif file
 plot(canopy_tif, main = "", breaks = breaks, col = color_breaks,
     box = FALSE, axes = FALSE, legend.shrink = 0.3)
 
+#Adding title to the .tif map
 title(main = "Canopy Coverage in Laval 2021", adj = 0.7, line = -2, cex.main = 1.2)
 
-# Flooding ----------------------------------------------------------------
-flooding <- st_read("D://Mcgill/can_cache/flood.gpkg")
+#Mapping out greenspaces (unused)
+ggplot() +
+  geom_sf(data = laval_csd, color = "black", fill = "#F8F8F8") +
+  geom_sf(data = greenspace_shp, color = "darkgreen", fill = "darkgreen") +
+  theme_minimal() +
+  theme(axis.line = element_blank(), axis.text = element_blank(),
+        axis.title = element_blank(), axis.ticks = element_blank(),
+        panel.grid = element_blank(), plot.title = element_text(hjust = 0.5),
+        legend.position = "bottom", legend.justification = "center")
 
+# Flooding ----------------------------------------------------------------
+#Vectors for the flooding
+flooding <- st_read("D://Mcgill/can_cache/flood.gpkg")
+curbcut_flooding <- st_read("/Users/justin/Documents/R/curbcut/zone_inondable_RCI_CDU_20240607_PG.shp") |> 
+  st_transform(crs = 4326)
+flooding0_2 <- curbcut_flooding |> 
+  filter(LIMITE == "Zone inondable 0-2 ans")
+flooding2_20 <- curbcut_flooding |> 
+  filter(LIMITE == "Zone inondable 2-20 ans")
+flooding20_100 <- curbcut_flooding |> 
+  filter(LIMITE == "Zone inondable 20-100 ans")
+
+#Bounding box of Laval
 laval_bbox <- st_bbox(laval_csd)
 
+#Mapping the Quebec given data
 ggplot() +
   geom_sf(data = laval_csd, fill = "grey", color = NA) +
   geom_sf(data = flooding, fill = "lightblue", color = NA) +
@@ -122,5 +153,24 @@ ggplot() +
         legend.position = "bottom", legend.justification = "center") +
   guides(fill = guide_legend(title.position = "top", title.hjust = 0.5, 
                              label.position = "bottom", keywidth = 3, keyheight = 0.5)) +
+  coord_sf(xlim = c(laval_bbox$xmin, laval_bbox$xmax),
+           ylim = c(laval_bbox$ymin, laval_bbox$ymax))
+
+#Flood map using the more granular data, not included in the first draft
+ggplot() +
+  geom_sf(data = mtlcma_sf, fill = "grey", color = "black") +
+  geom_sf(data = laval_csd, fill = "#F8F8F8", color = "black") +
+  geom_sf(data = flooding20_100, fill = "#bdc9e1",color = NA) +
+  geom_sf(data = flooding2_20, fill = "#74a9cf",color = NA) +
+  geom_sf(data = flooding0_2, fill = "#045a8d",color = NA) +
+  geom_sf(data = laval_csd, fill = NA, color = "black") +
+  labs(title = "Flood Zones in Laval 2023",
+       fill = "Level of Vulnerability") +
+  theme_minimal() +
+  theme(axis.line = element_blank(), axis.text = element_blank(),
+        panel.background = element_rect(fill = "#cedbdc"),
+        axis.title = element_blank(), axis.ticks = element_blank(),
+        panel.grid = element_blank(), plot.title = element_text(hjust = 0.5),
+        legend.position = "bottom", legend.justification = "center") +
   coord_sf(xlim = c(laval_bbox$xmin, laval_bbox$xmax),
            ylim = c(laval_bbox$ymin, laval_bbox$ymax))
