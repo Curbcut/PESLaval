3bd35323eebdedd71c738ec346c608362a54c618
Justin Le <justin.le2@mail.mcgill.ca>
2024-07-19 12:17:44 -0400

    New graph of 5-year incremental comparison

--- R/13_scripts/education.R
+++ R/13_scripts/education.R
@@ -173,10 +173,8 @@ ggplot(hc15, aes(x = education, y = count, fill = factor(Type, levels = c("Total
 hc25v <- c("total" = "v_CA21_5865", "total_m" = "v_CA21_5866", "total_f" = "v_CA21_5867",
               "none" = "v_CA21_5868", "none_m" = "v_CA21_5869", "none_f" = "v_CA21_5870",
               "sec" = "v_CA21_5871", "sec_m" = "v_CA21_5872", "sec_f" = "v_CA21_5873",
-              "app" = "v_CA21_5880", "app_m" = "v_CA21_5881", "app_f" = "v_CA21_5882",
-              "col" = "v_CA21_5889", "col_m" = "v_CA21_5890", "col_f" = "v_CA21_5891",
-              "uni" = "v_CA21_5892", "uni_m" = "v_CA21_5893", "uni_f" = "v_CA21_5894",
-              "bac" = "v_CA21_5895", "bac_m" = "v_CA21_5896", "bac_f" = "v_CA21_5897")
+              "psec" = "v_CA21_5877", "psec_m" = "v_CA21_5878", "psec_f" = "v_CA21_5879",
+              "uni" = "v_CA21_5895", "uni_m" = "v_CA21_5896", "uni_f" = "v_CA21_5897")
 
 #Grabbing actual data, and cleaning it up for use
 hc25 <- get_census(
@@ -184,39 +182,110 @@ hc25 <- get_census(
   regions = list(CSD = (2465005)),
   level = "CSD",
   vectors = hc25v) |> 
-  select(none, none_m, none_f, sec, sec_m, sec_f, app,
-         app_m, app_f, col, col_m, col_f, uni, uni_m, uni_f, bac, bac_m, bac_f) |> 
+  select(none, none_m, none_f, sec, sec_m, sec_f, psec, psec_m, psec_f, uni, uni_m, uni_f) |> 
   pivot_longer(cols = everything(),
                names_to = "variable",
                values_to = "count") |> 
   mutate(education = case_when(str_starts(variable, "none") ~ "Aucun certificat, diplôme ou grade",
                                str_starts(variable, "sec") ~ "Diplôme d'études secondaires",
-                               str_starts(variable, "app") ~ "Certificat ou diplôme d'apprenti",
-                               str_starts(variable, "col") ~ "Diplôme d'un collège",
-                               str_starts(variable, "uni") ~ "Diplôme universitaire inférieur au baccalauréat",
-                               str_starts(variable, "bac") ~ "Diplôme universitaire",
+                               str_starts(variable, "psec") ~ "Certificat ou diplôme postsecondaire inférieur au baccalauréat",
+                               str_starts(variable, "uni") ~ "Diplôme universitaire",
                                TRUE ~ "Other"),
-         Type = case_when(str_ends(variable, "_m") ~ "Male",
-                          str_ends(variable, "_f") ~ "Female",
-                          TRUE ~ "Total")) |> 
-  mutate(Type = factor(Type, levels = c("Total", "Female", "Male")),
+         Type = case_when(str_ends(variable, "_m") ~ "Hommes",
+                          str_ends(variable, "_f") ~ "Femmes",
+                          TRUE ~ "Total")) |>
+  mutate(Type = factor(Type, levels = c("Total", "Femmes", "Hommes")),
          education = factor(education,
                             levels = c("Aucun certificat, diplôme ou grade", "Diplôme d'études secondaires",
-                                       "Certificat ou diplôme d'apprenti", "Diplôme d'un collège",
-                                       "Diplôme universitaire inférieur au baccalauréat",
+                                       "Certificat ou diplôme postsecondaire inférieur au baccalauréat",
                                        "Diplôme universitaire")),
-         prop = paste0(round(count / sum(count) * 100, 1), "%"))
-#Autowrapping the text so it's readable
-hc25$education <- str_wrap(hc25$education, width = 25)
+         prop = paste0(round(count / 228815 * 100, 1), "%"))
 
 
 #Creating to bar graph
-ggplot(hc25, aes(x = education, y = count, fill = factor(Type, levels = c("Total", "Male", "Female")))) +
+ggplot(hc25, aes(x = education, y = count, fill = Type)) +
   geom_bar(stat = "identity", position = "dodge", aes(group = variable)) +
+  geom_text(aes(label = prop, y = count), position = position_dodge(width = 0.9), vjust = 1.5, size = 3.5, color = "white") +
   labs(x = "", y = "Personnes") +
   scale_fill_manual(values = c("Total" = "#73AD80",
-                               "Male" = "#A3B0D1",
-                               "Female" = "#CD718C")) +
+                               "Hommes" = "#A3B0D1",
+                               "Femmes" = "#CD718C")) +
+  scale_x_discrete(labels = c("Aucun certificat, diplôme ou\ngrade",
+                              "Diplôme d'études secondaires",
+                              "Certificat ou diplôme\npostsecondaire inférieur\nau baccalauréat",
+                              "Diplôme universitaire")) +
   theme_minimal() +
   theme(legend.position = "bottom", legend.title = element_blank(),
-        axis.text.x = element_text(angle = 45, hjust = 1))
+        axis.text.x = element_text(angle = 40, hjust = 1))
+
+# Composition 2006-2021 ---------------------------------------------------
+comp21v <- c("total" = "v_CA21_5865", "none" = "v_CA21_5868", "sec" = "v_CA21_5871",
+             "psec" = "v_CA21_5877", "uni" = "v_CA21_5895")
+comp16v <- c("total" = "v_CA16_5096", "none" = "v_CA16_5099", "sec" = "v_CA16_5102",
+             "psec" = "v_CA16_5105", "uni" = "v_CA16_5123")
+comp11v <- c("total" = "v_CA11N_1801", "none" = "v_CA11N_1804", "sec" = "v_CA11N_1807",
+             "psec" = "v_CA11N_1810", "uni" = "v_CA11N_1822")
+comp06v <- c("total" = "v_CA06_1248", "none" = "v_CA06_1249", "sec" = "v_CA06_1251",
+             "cert" = "v_CA06_1250", "uni" = "v_CA06_1256")
+
+comp21 <- get_census(dataset = "CA21",
+                      regions = list(CSD = (2465005)),
+                      level = "CSD",
+                      vectors = comp21v) |> 
+  mutate(Year = 2021) |> 
+  select(Year, total, none, sec, psec, uni)
+
+comp16 <- get_census(dataset = "CA16",
+                     regions = list(CSD = (2465005)),
+                     level = "CSD",
+                     vectors = comp16v) |> 
+  mutate(psec = psec - uni) |> 
+  mutate(Year = 2016) |> 
+  select(Year, total, none, sec, psec, uni)
+
+comp11 <- get_census(dataset = "CA11",
+                     regions = list(CSD = (2465005)),
+                     level = "CSD",
+                     vectors = comp11v) |> 
+  mutate(psec = psec - uni) |> 
+  mutate(Year = 2011) |> 
+  select(Year, total, none, sec, psec, uni)
+
+comp06 <- get_census(dataset = "CA06",
+                     regions = list(CSD = (2465005)),
+                     level = "CSD",
+                     vectors = comp06v) |> 
+  mutate(psec = cert - sec - uni) |> 
+  mutate(Year = 2006) |> 
+  select(Year, total, none, sec, psec, uni)
+
+comp <- bind_rows(comp06, comp11, comp16, comp21) |> 
+  mutate(Year = as.factor(Year),
+         none = round(none / total * 100, 1),
+         sec = round(sec / total * 100, 1),
+         psec = round(psec / total * 100, 1),
+         uni = round(uni / total * 100, 1)) |> 
+  select(-total) |> 
+  pivot_longer(cols = -Year, names_to = "level", values_to = "prop") |> 
+  mutate(prop_per = paste0(prop, "%")) |> 
+  mutate(level = factor(level, levels = c("uni", "psec", "sec", "none"))) |> 
+  group_by(Year) |> 
+  mutate(cumulative = cumsum(prop) - 0.5 * prop) |> 
+  ungroup()
+
+ggplot(comp, aes(x = Year, y = prop, fill = level)) +
+  geom_bar(stat = "identity", width = 0.5) +
+  geom_text(aes(label = prop_per, y = cumulative), vjust = 0.5, color = "white", size = 3) +
+  labs(x = "Année") +
+  scale_fill_manual(values = c("none" = "#252c3d", "sec" = "#3d4a66",
+                               "psec" = "#6C83B5", "uni" = "#98A8CB"),
+                    labels = c("none" = "Aucun certificat, diplôme ou grade",
+                               "sec" = "Diplôme d'études secondaires ou\nattestation d'équivalence",
+                               "psec" = "Certificat ou diplôme d’études\npostsecondaires inférieur au baccalauréat",
+                               "uni" = "Baccalauréat ou grade supérieur")) +
+  theme_minimal() +
+  theme(legend.title = element_blank(), legend.position = "bottom",
+        plot.title = element_blank(), axis.title.y = element_blank(),
+        axis.text.y = element_blank(), axis.ticks.y = element_blank(),
+        axis.title.x = element_blank(), axis.ticks.x = element_blank()) +
+  guides(fill = guide_legend(ncol = 2))
